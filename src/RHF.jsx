b14c8a172d62d813dc5f0a025626ea62e6e2c123
AnaisLaguna <annslaguna@gmail.com>
2024-06-24 17:03:02 -0600

    react-todo completo

--- src/RHF.jsx
+++ src/RHF.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import {useForm} from 'react-hook-form';
+import clsx from "clsx";
 
-export default function App() {
+export default function RHF() {
   //lista de todos (se guarda el valor del input )
 const [todos, setTodos] = useState([]);
-const [text, setText] = useState("");
+//const [text, setText] = useState("");
 
-function addTodo() {
- setTodos([ ...todos,text]);
-}
+const {
+    register, 
+    handleSubmit, 
+    formState: {errors, isValid, isSubmitted }, 
+    reset,
+} = useForm();
+
 
 function removeTodo(indexToRemove){
   //todos.splice(indexToRemove, 1)
@@ -18,30 +24,53 @@ function removeTodo(indexToRemove){
   setTodos(newTodos);
   
 }
+/**
+ * data: es un objeto que contiene en cada propiedad el valor de cada input del formulario
+ * data.todo -> valor del input name="todo"
+ */
 
-function onSubmit(event){
-event.preventDefault();
-addTodo();
-setText("");
+function onSubmit(data) {
+    console.log("onSumbit");
+    setTodos([...todos, data.todo]);
+    reset();
+
 }
 
   return (
     <main className="w-full min-h-screen flex flex-col">
+        <p className="w-full bg-teal-500  text-black font-bold text-center p-2"> 
+            TO-DO React-hook-form 
+        </p>
       <form 
       className="flex flex-row gap-2 justify-center p-5"
-      onSubmit={onSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       >
         <input 
-          name="todo"
           type="text" 
-          className="p-2 rounded-md text-black w-full max-w-screen-sm"
+          className={clsx("p-2 rounded-md text-black w-full max-w-screen-sm" ,{
+            'border-2 border-red-500 bg-red-300' : errors.todo,
+          })}
           placeholder="Ingresa una tarea" 
-          value={text}
-          onChange={(event) => setText(event.target.value)}
           required
+          {...register("todo", {
+            required: { value: true, message: "campo requerido" },
+            minLength: { value: 3, message: "Minimo 3 caracteres" },
+            maxLength: { value: 180, message: "maximo 180 caracteres" },
+          })}
         />
-        <button className="bg-white text-black px-3 rounded"> + Agregar</button>
+        <button 
+            className="text-black px-3 rounded bg-stone-400"
+            disabled={isSubmitted ? !isValid: false}
+        > 
+            + Agregar
+        </button>
       </form>
+
+        {errors.todo && (
+      < p className="text-red-500 text-center text-sm font-semibold  ">
+        {errors.todo?.message}
+      </p>
+      )}
 
       <div className="max-w-screen-sm w-full mx-auto p-4 flex flex-col gap-1">
         {
